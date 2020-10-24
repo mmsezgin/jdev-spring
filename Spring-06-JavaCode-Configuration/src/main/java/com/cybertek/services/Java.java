de00166307bbbf3a98ca2361ba9b86c574365661
cybertekschool <github@cybertekschool.com>
2020-10-24 14:43:15 -0400

    Java Code - Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/services/Java.java
@@ -1,12 +1,21 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import com.cybertek.interfaces.ExtraSessions;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-@Component
+
 public class Java implements Course {
 
+    private ExtraSessions extraSessions;
+
+
+    public Java(ExtraSessions extraSessions) {
+        this.extraSessions = extraSessions;
+    }
+
     public void getTeachingHours() {
-        System.out.println("Weekly teaching hours 30");
+        System.out.println("Weekly teaching hours: " + (30 + extraSessions.getHours()));
     }
 }
