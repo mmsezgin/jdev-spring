80a072ac3bcb0bc6542c7d712f846509f874d6d8
cybertekschool <github@cybertekschool.com>
2020-10-24 14:50:22 -0400

    Java Code - Configuration

--- Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
+++ Spring-06-JavaCode-Configuration/src/main/java/com/cybertek/configs/CybertekAppConfig.java
@@ -7,9 +7,11 @@ import com.cybertek.services.Selenium;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan("com.cybertek")
+@PropertySource("classpath:application.properties")
 public class CybertekAppConfig {
 
     @Bean
@@ -21,18 +23,19 @@ public class CybertekAppConfig {
     public Selenium selenium(){
         return new Selenium();
     }
-  /*
-    @Bean
+/*
+    //@Bean
     public OfficeHours officeHours(){
         return new OfficeHours();
     }
 
-   */
+ */
 
-    @Bean
+    //@Bean
     public ExtraSessions extraSessions(){
         return new OfficeHours();
     }
+
 
 
 }
