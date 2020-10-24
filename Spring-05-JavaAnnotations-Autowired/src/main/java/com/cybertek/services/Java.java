1c39143286b02b68d31c13d0295ad62c1fad3e42
cybertekschool <github@cybertekschool.com>
2020-10-24 11:38:56 -0400

    Java Annotations - Autowired

--- Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
+++ Spring-05-JavaAnnotations-Autowired/src/main/java/com/cybertek/services/Java.java
@@ -1,13 +1,35 @@
 package com.cybertek.services;
 
 import com.cybertek.interfaces.Course;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Java implements Course {
 
+    @Autowired
     private OfficeHours officeHours;
+/*
+    //Constructor Injection
+    @Autowired
+    public Java(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
 
+ */
+
+    public OfficeHours getOfficeHours() {
+        return officeHours;
+    }
+/*
+    //Setter Injection
+    @Autowired
+    public void setOfficeHours(OfficeHours officeHours) {
+        this.officeHours = officeHours;
+    }
+
+ */
 
     @Override
     public void getTeachingHours() {
