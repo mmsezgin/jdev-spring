aa7adc002019be544814ae52ee0871b509d57c3d
cybertekschool <github@cybertekschool.com>
2020-11-14 11:52:16 -0500

    SpringBoot MVC - Model - View

--- Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
+++ Spring-08-SpringBoot-MVC-Model-View(Thymeleaf)/src/main/java/com/cybertek/controller/StudentController.java
@@ -5,6 +5,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 @Controller
 @RequestMapping("/student")
 public class StudentController {
@@ -19,6 +24,18 @@ public class StudentController {
        model.addAttribute("subject",subject);
 
        //create some random studentid (0-1000) and show it in your UI
+        int studentId = new Random().nextInt(1000);
+        model.addAttribute("id",studentId);
+
+        List<Integer> numbers = new ArrayList<>();
+        numbers.add(4);
+        numbers.add(5);
+        numbers.add(7);
+        model.addAttribute("numbers",numbers);
+
+        //print your birthday
+        LocalDate birthday = LocalDate.now().minusYears(42);
+        model.addAttribute("birthday",birthday);
 
 
        return "student/welcome";
