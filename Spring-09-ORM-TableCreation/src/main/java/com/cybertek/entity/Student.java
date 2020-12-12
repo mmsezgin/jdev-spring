e27ffba6593fea42d3e6aa8086bd0bc57e8f7e4e
ozzycybertek <ozzy@cybertekschool.com>
2020-12-12 13:43:24 -0500

    ORM-Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -1,12 +1,13 @@
 package com.cybertek.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "students")
 public class Student {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;  //first_name
     private String lastName;
