6bb3167b374c9a70e40c6639af20c9abccd97c43
ozzycybertek <ozzy@cybertekschool.com>
2020-12-12 13:49:19 -0500

    ORM-Table Creation

--- Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
+++ Spring-09-ORM-TableCreation/src/main/java/com/cybertek/entity/Student.java
@@ -9,8 +9,12 @@ public class Student {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "studentFirstName")
     private String firstName;  //first_name
+    @Column(name = "studentLastName")
     private String lastName;
+    @Column(name = "studentEmailAddres")
     private String email;
 
 }
