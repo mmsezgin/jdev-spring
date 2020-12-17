f5b4e8dc81d27b5c97708cf7ad9fedc0fb4a410e
ozzycybertek <ozzy@cybertekschool.com>
2020-12-16 19:54:39 -0500

    ORM-Mapping-OneToMany

--- Spring-09-ORM-OneToMany/src/main/java/com/cybertek/bootstrap/DataGenerator.java
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/bootstrap/DataGenerator.java
@@ -1,5 +1,7 @@
 package com.cybertek.bootstrap;
 
+import com.cybertek.entity.Address;
+import com.cybertek.entity.Person;
 import com.cybertek.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,6 +15,17 @@ public class DataGenerator implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        Person p1 = new Person("Mike","Smith");
+        Person p2 = new Person("Ozzy","Kmith");
+        Person p3 = new Person("Tedd","Bbith");
+
+        Address a1 = new Address("King St","22042");
+        Address a2 = new Address("Elm St","22102");
+        Address a3 = new Address("Java St","22036");
+
+
+
 
 
 
