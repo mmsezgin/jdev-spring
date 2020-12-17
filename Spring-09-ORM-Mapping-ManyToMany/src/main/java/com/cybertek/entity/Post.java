a1f7fb04b53448b649f2b0ef2c021dfcfd5bb693
ozzycybertek <ozzy@cybertekschool.com>
2020-12-16 20:38:53 -0500

    ORM-Mapping-ManyToMany

--- Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
+++ Spring-09-ORM-Mapping-ManyToMany/src/main/java/com/cybertek/entity/Post.java
@@ -22,4 +22,8 @@ public class Post {
     private String title;
     private String description;
 
+    public Post(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
 }
