805a004b1ecc94305feac968d3091f59236f2b4e
ozzycybertek <ozzy@cybertekschool.com>
2020-12-16 19:52:00 -0500

    ORM-Mapping-OneToMany

--- Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
+++ Spring-09-ORM-OneToMany/src/main/java/com/cybertek/entity/Person.java
@@ -1,9 +1,16 @@
 package com.cybertek.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@NoArgsConstructor
+@Getter
+@Setter
 public class Person {
 
     @Id
@@ -13,12 +20,15 @@ public class Person {
     private String firstName;
     private String lastName;
 
-    @OneToMany(mappedBy = "person")
-    private List<Address> addresses;
-
-//    @OneToMany
-//    @JoinColumn(name="person_id")
+//    @OneToMany(mappedBy = "person")
 //    private List<Address> addresses;
 
+    @OneToMany
+    @JoinColumn(name="person_id")
+    private List<Address> addresses;
 
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
