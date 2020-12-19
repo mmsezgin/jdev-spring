661dab06dbe6807217ba3f314b504bea6fa7a9b1
ozzycybertek <ozzy@cybertekschool.com>
2020-12-19 11:22:06 -0500

    ORM-Lab-CinemaApp

---
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/entity/Cinema.java
@@ -0,0 +1,19 @@
+package com.cybertek.entity;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+public class Cinema extends BaseEntity {
+
+    private String name;
+    private String sponsoredName;
+
+
+}
