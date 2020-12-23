3c8706450e23d0be7d283da9b391d81a16c75672
ozzycybertek <ozzy@cybertekschool.com>
2020-12-22 20:52:15 -0500

    ORM-Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -1,4 +1,47 @@
 package com.cybertek.repository;
 
-public interface EmployeeRepository {
+import com.cybertek.entity.Employee;
+import org.apache.tomcat.jni.Local;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Integer> {
+
+    //Display all employees with email address ''
+    List<Employee> findByEmail(String email);
+
+    //Display all employees with first name '' and last name '', also show all employees with a email address ''
+    List<Employee> findByFirstNameAndLastNameOrEmail(String firstName,String lastName,String email);
+
+    //Display all employees that first name is not ''
+    List<Employee> findByFirstNameIsNot(String firstName);
+
+    //Display all employees where last name starts with ''
+    List<Employee> findByLastNameStartsWith(String pattern);
+
+    //Display all employees with salaries higher than ''
+    List<Employee> findBySalaryGreaterThan(Integer salary);
+
+    //Display all employees with salaries less than ''
+    List<Employee> findBySalaryLessThanEqual(Integer salary);
+
+    //Display all employees that has been hired between '' and ''
+    List<Employee> findByHireDateBetween(LocalDate startDate, LocalDate endDate);
+
+    //Display all employees where salaries greater and equal to '' in order
+
+
+
+
+
+
+
+
+
+
+
 }
--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/repository/RegionRepository.java
@@ -30,4 +30,6 @@ public interface RegionRepository extends JpaRepository<Region,Integer> {
 
 
 
+
+
 }
