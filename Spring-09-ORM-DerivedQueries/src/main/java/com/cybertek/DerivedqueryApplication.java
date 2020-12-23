1968872d75a631ca613d964331c39cc25217a58c
ozzycybertek <ozzy@cybertekschool.com>
2020-12-22 20:23:21 -0500

    ORM-Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
@@ -1,5 +1,6 @@
 package com.cybertek;
 
+import com.cybertek.repository.DepartmentRepository;
 import com.cybertek.repository.RegionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -13,6 +14,8 @@ public class DerivedqueryApplication {
 
 	@Autowired
 	RegionRepository regionRepository;
+	@Autowired
+	DepartmentRepository departmentRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DerivedqueryApplication.class, args);
@@ -30,15 +33,21 @@ public class DerivedqueryApplication {
 		System.out.println("findByCountryContainingOrderByCountry: " + regionRepository.findByCountryContainingOrderByCountry("Asia"));
 		System.out.println("findTopBy2ByCountry: " + regionRepository.findTop2ByCountry("Canada"));
 
-
-
-
-
-
-
-
-
 		System.out.println("---------Regions End------------");
+
+	}
+
+	@PostConstruct
+	public void testDepartments(){
+
+		System.out.println("---------Department start------------");
+
+		System.out.println("findByDepartment: " + departmentRepository.findByDepartment("Toys"));
+		System.out.println("findByDivision: " + departmentRepository.findByDivision("Outdoors"));
+		System.out.println("findByDivisionEndingWith: " + departmentRepository.findByDivisionEndingWith("ics"));
+		System.out.println("findDistinctTop3ByDivisionContains: " + departmentRepository.findDistinctTop3ByDivisionContains("Hea"));
+
+		System.out.println("---------Department End------------");
 
 	}
 
