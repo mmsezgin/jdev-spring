c369fab75645cac23aa2b8b04baba2e9601ed8f6
ozzycybertek <ozzy@cybertekschool.com>
2020-12-22 19:59:40 -0500

    ORM-Derived Queries

--- Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
+++ Spring-09-ORM-DerivedQueries/src/main/java/com/cybertek/DerivedqueryApplication.java
@@ -1,13 +1,42 @@
 package com.cybertek;
 
+import com.cybertek.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DerivedqueryApplication {
 
+	@Autowired
+	RegionRepository regionRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(DerivedqueryApplication.class, args);
+	}
+
+	@PostConstruct
+	public void testRegions(){
+
+		System.out.println("---------Regions start------------");
+
+		System.out.println("findByCountry: " + regionRepository.findByCountry("Canada"));
+		System.out.println("findDistinctByCountry: " + regionRepository.findDistinctByCountry("Canada"));
+		System.out.println("findDistinctByCountry: " + regionRepository.findByCountryContaining("United"));
+
+
+
+
+
+
+
+
+
+		System.out.println("---------Regions End------------");
+
 	}
 
 }
