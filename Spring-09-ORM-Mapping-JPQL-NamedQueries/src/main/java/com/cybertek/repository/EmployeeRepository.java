872962216d3e6d1b5bd71daae5c73d19ae9494d0
ozzycybertek <ozzy@cybertekschool.com>
2020-12-23 19:14:50 -0500

    ORM-JPQL-NamedQueries

--- Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
+++ Spring-09-ORM-Mapping-JPQL-NamedQueries/src/main/java/com/cybertek/repository/EmployeeRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
@@ -13,6 +15,22 @@ public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     @Query("SELECT e.salary FROM Employee e WHERE e.email = 'dtrail8@tamu.edu' ")
     Integer getEmployeeSalary();
+
+    //single bind paramater
+    @Query("SELECT e FROM Employee e WHERE e.email = ?1 ")
+    Optional<Employee> getEmployeeByEmail(String email);
+
+    //multiple bind parameter
+    @Query("SELECT e FROM Employee e WHERE e.email=?1 AND e.salary=?2")
+    Employee getEmployeeByEmailAndSalary(String email,int salary);
+
+
+
+
+
+
+
+
 
 
 }
