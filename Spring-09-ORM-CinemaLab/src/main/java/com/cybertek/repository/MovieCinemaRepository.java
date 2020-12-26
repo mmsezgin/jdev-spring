f2c2ae3fc80e09bad3d93a7ac2a757e82240c861
ozzycybertek <ozzy@cybertekschool.com>
2020-12-26 12:03:22 -0500

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieCinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieCinemaRepository.java
@@ -2,7 +2,12 @@ package com.cybertek.repository;
 
 import com.cybertek.entity.MovieCinema;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MovieCinemaRepository extends JpaRepository<MovieCinema,Long> {
@@ -11,21 +16,33 @@ public interface MovieCinemaRepository extends JpaRepository<MovieCinema,Long> {
 
     //Write a derived query to read movie cinema with id
 
+    Optional<MovieCinema> findById(Long id);
+
     //Write a derived query to count all movie cinemas with a specific cinema id
+    Integer countAllByCinemaId(Long cinemaId);
 
     //Write a derived query to count all movie cinemas with a specific movie id
+    Integer countAllByMovieId(Integer movieId);
 
     //Write a derived query to list all movie cinemas with higher than a specific date
+    List<MovieCinema> findAllByDateTimeAfter(LocalDateTime dateTime);
 
     //Write a derived query to find the top 3 expensive movies
+    List<MovieCinema> findFirst3ByOrderByMoviePriceAsc();
 
     //Write a derived query to list all movie cinemas that contain a specific movie name
+    List<MovieCinema> findAllByMovieNameContaining(String name);
 
     //Write a derived query to list all movie cinemas in a specific location
+    List<MovieCinema> findAllByCinemaLocationName(String name);
 
     // ------------------- JPQL QUERIES ------------------- //
 
-    //Write a JPQL query to list all movie cinemas with higher than a specific date
+    //Write a JPQL query to list all movie cinemas with after a specific date
+
+    @Query("SELECT mc FROM MovieCinema mc WHERE mc.dateTime > ?1")
+    List<MovieCinema> fetchAllWithHigherSpecificDateJPQL(LocalDateTime dateTime);
+
 
     // ------------------- Native QUERIES ------------------- //
 
