b44a2cc5efed399aa914696543cc14be373c9161
ozzycybertek <ozzy@cybertekschool.com>
2020-12-26 11:35:39 -0500

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/CinemaRepository.java
@@ -32,14 +32,21 @@ public interface CinemaRepository extends JpaRepository<Cinema,Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c WHERE c.id=?1")
+    String fetchByIdJPQL(Integer id);
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query to read all cinemas by location country
+    @Query(value = "SELECT * FROM cinema c JOIN location l on l.id=c.location_id WHERE l.country=?1",nativeQuery = true)
+    List<Cinema> retrieveAllByLocationCountry(String locationCountry);
 
     //Write a native query to read all cinemas by name or sponsored name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE concat('%',?1,'%') OR sponsored_name ILIKE concat('%',?1,'%')",nativeQuery = true )
+    List<Cinema> retrieveAllByNameOrSponsoredName(String pattern);
 
     //Write a native query to sort all cinemas by name
+
 
     //Write a native query to distinct all cinemas by sponsored name
 
