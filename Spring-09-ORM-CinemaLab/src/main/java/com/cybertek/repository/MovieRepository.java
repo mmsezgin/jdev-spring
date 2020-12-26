a6fbfe5a2bceb3ea699193734adfd1b1817bc2ad
ozzycybertek <ozzy@cybertekschool.com>
2020-12-26 13:55:32 -0500

    ORM-JPQL-Cinema Lab

--- Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieRepository.java
+++ Spring-09-ORM-CinemaLab/src/main/java/com/cybertek/repository/MovieRepository.java
@@ -4,6 +4,7 @@ import com.cybertek.entity.Movie;
 import com.cybertek.enums.MovieState;
 import com.cybertek.enums.MovieType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -35,19 +36,43 @@ public interface MovieRepository extends JpaRepository<Movie,Long> {
     // ------------------- JPQL QUERIES ------------------- //
 
     //Write a JPQL query to list all movies between a range of prices
+    @Query("SELECT m FROM Movie m WHERE m.price BETWEEN ?1 AND ?2")
+    List<Movie> fetchAllBetweenPricesRange(BigDecimal price1,BigDecimal price2);
 
     //Write a JPQL query that returns all movie names
+    @Query("SELECT DISTINCT m.name FROM Movie m")
+    List<String> fetchAllMovieNames();
 
     // ------------------- Native QUERIES ------------------- //
 
     //Write a native query that returns a movie by name
+    @Query(value = "SELECT * FROM movie WHERE name = ?1",nativeQuery = true)
+    Optional<Movie> retrieveByName(String name);
 
     //Write a native query that return the list of movies in a specific range of prices
+    @Query(value = "SELECT * FROM movie WHERE price BETWEEN ?1 AND ?2",nativeQuery = true)
+    List<Movie> retrieveByPriceRange(BigDecimal price1,BigDecimal price2);
 
     //Write a native query to return all movies where duration exists in the range of duration
+    @Query(value = "SELECT * FROM movie WHERE duration IN ?1",nativeQuery = true)
+    List<Movie> retrieveByDurationInRange(List<Integer> durations);
 
     //Write a native query to list the top 5 most expensive movies
+    @Query(value = "SELECT * FROM movie ORDER BY price DESC LIMIT 5",nativeQuery = true)
+    List<Movie> top5ExpensiveMovie();
+
 
 
 
 }
+
+
+
+
+
+
+
+
+
+
+
