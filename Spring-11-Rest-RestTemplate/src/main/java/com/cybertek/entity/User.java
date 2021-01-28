9f2e29d25ebc83f0fb3826e6b24e1069dcc3ff8f
ozzycybertek <ozzy@cybertekschool.com>
2021-01-27 19:35:52 -0500

    Spring REST - Jackson

--- Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/entity/User.java
+++ Spring-11-Rest-RestTemplate/src/main/java/com/cybertek/entity/User.java
@@ -15,6 +15,7 @@ import javax.persistence.*;
 @Table(name="users")
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
