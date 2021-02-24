ed70adb52db814d5213f69bf8ae4cca876ae45db
ozzycybertek <ozzy@cybertekschool.com>
2021-02-23 20:58:43 -0500

    Spring AOP

--- Spring-12-AOP/src/main/java/com/cybertek/aspects/LoggingAspect.java
+++ Spring-12-AOP/src/main/java/com/cybertek/aspects/LoggingAspect.java
@@ -1,13 +1,17 @@
 package com.cybertek.aspects;
 
 import com.cybertek.controller.ProductController;
+import com.cybertek.entity.Product;
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Aspect
 @Configuration
@@ -31,21 +35,21 @@ public class LoggingAspect {
 //
 
     //execution
-    @Pointcut("execution(* com.cybertek.controller.ProductController.up*(..))")
-    private void anyUpdateOperation(){}
-
-    @Pointcut("execution(* com.cybertek.repository.ProductRepository.findById(Long))")
-    private void anyProductRepositoryFindById(){}
-
-    @Before("anyProductRepositoryFindById()")
-    public void beforeProductRepoAdvice(JoinPoint joinPoint){
-        logger.info("Before(findById) -> Method {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
-    }
-
-    @Before("anyUpdateOperation()")
-    public void beforeControllerAdvice(JoinPoint joinPoint){
-        logger.info("Before -> Method {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
-    }
+//    @Pointcut("execution(* com.cybertek.controller.ProductController.up*(..))")
+//    private void anyUpdateOperation(){}
+//
+//    @Pointcut("execution(* com.cybertek.repository.ProductRepository.findById(Long))")
+//    private void anyProductRepositoryFindById(){}
+//
+//    @Before("anyProductRepositoryFindById()")
+//    public void beforeProductRepoAdvice(JoinPoint joinPoint){
+//        logger.info("Before(findById) -> Method {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
+//    }
+//
+//    @Before("anyUpdateOperation()")
+//    public void beforeControllerAdvice(JoinPoint joinPoint){
+//        logger.info("Before -> Method {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
+//    }
 
     //within
     @Pointcut("within(com.cybertek.controller..*)")
@@ -58,6 +62,35 @@ public class LoggingAspect {
     public void beforeControllerAdvice2(JoinPoint joinPoint){
         logger.info("Before -> Method : {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
     }
+
+    //annotation
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.DeleteMapping)")
+    private void anyDeleteProductOperation(){}
+
+    @Before("anyDeleteProductOperation()")
+    public void beforeControllerAdvice(JoinPoint joinPoint){
+        logger.info("Before -> Method : {} - Arguments : {} - Target : {}",joinPoint,joinPoint.getArgs(),joinPoint.getTarget());
+    }
+
+    //after returning
+    @Pointcut("@annotation(org.springframework.web.bind.annotation.GetMapping)")
+    private void anyGetProductOperation(){}
+
+    @AfterReturning(pointcut = "anyGetProductOperation()",returning = "results")
+    public void afterReturningControllerAdvice(JoinPoint joinPoint, Product results){
+        logger.info("After Returning(Mono Result) -> Method : {} - results :{}",joinPoint.getSignature().toShortString(),results);
+    }
+
+    @AfterReturning(pointcut = "anyGetProductOperation()",returning = "results")
+    public void afterReturningControllerAdvice2(JoinPoint joinPoint, List<Product> results){
+        logger.info("After Returning(List Result) -> Method : {} - results :{}",joinPoint.getSignature().toShortString(),results);
+    }
+
+
+
+
+
+
 
 
 
