a3f12df3844f6b9aa4df64d009e9ca8d38e101f6
ozzycybertek <ozzy@cybertekschool.com>
2021-02-27 13:41:26 -0500

    Unit Testing - Junit5

--- Spring-13-UnitTesting-Junit5/src/test/java/CalculatorParameterizedTest.java
+++ Spring-13-UnitTesting-Junit5/src/test/java/CalculatorParameterizedTest.java
@@ -42,6 +42,23 @@ public class CalculatorParameterizedTest {
         return arr;
     }
 
+    @ParameterizedTest
+    @CsvSource({
+            "10,20,30",
+            "20,20,40",
+            "30,20,100"
+    })
+    void testCase5(int num1,int num2,int result){
+        Assertions.assertEquals(result,Calculator.add(num1,num2));
+    }
+
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/sample-data.csv",numLinesToSkip = 1)
+    void testCase6(int num1,int num2,int result){
+        Assertions.assertEquals(result,Calculator.add(num1,num2));
+    }
+
 
 
 
