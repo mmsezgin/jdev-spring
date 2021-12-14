3fca8adb6c16cb7815021b1ef803222b8f773338
Toshiba <mehmetsezginm@gmail.com>
2021-12-14 14:11:35 +0000

    Authentication and Authorization into the database created/updated

--- Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/entity/User.java
+++ Spring-10-Security-HttpBasic-Database/src/main/java/com/cybertek/entity/User.java
@@ -9,18 +9,19 @@ import java.util.Arrays;
 import java.util.List;
 
 @Entity
-@Table(name = "users")
+@Table(name = "users")  //change table name to users, because user is a reserved work in postgres
 @NoArgsConstructor
 @Getter
 public class User {
+    //In this class, I will create authentication and authorization in this class.
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // Autogenerate primary key
     private  Long id;
 
     private String username;
     private String password;
-    private int active;
-    private String roles;
+    private int active;  // Is the user active?
+    private String roles; // some designers put roles and permission under a separate Authority class
     private String permissions;
 
     public User(String username, String password,String roles, String permissions) {
