df2d4493c83952aed9c292daf5867ddbc8aba98f
Toshiba <mehmetsezginm@gmail.com>
2021-12-23 01:05:28 +0000

    @JsonIgnoreProperties(value={"hibernateLazyInitializer"},ignoreUnknown = true)

--- Spring-11-Rest-Jackson/src/main/java/com/cybertek/entity/User.java
+++ Spring-11-Rest-Jackson/src/main/java/com/cybertek/entity/User.java
@@ -14,11 +14,14 @@ import javax.persistence.*;
 @Setter
 @NoArgsConstructor
 @Table(name = "user_account")
+// Whenever FetchType.Lazy Spring automatically adds hibernateLazyInitializer
+// Developers use below line when they used hibernateLazyInitializer
 @JsonIgnoreProperties(value={"hibernateLazyInitializer"},ignoreUnknown = true)
 public class User extends BaseEntity {
 
     private String email;
-
+    // When I post (setter) a user, I should write password
+    // When retrieving (getter) user, do not show me the password
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
 
@@ -26,7 +29,7 @@ public class User extends BaseEntity {
 
     @OneToOne(fetch = FetchType.LAZY,cascade = {CascadeType.PERSIST,CascadeType.MERGE})
     @JoinColumn(name = "account_details_id")
-    @JsonManagedReference
+    @JsonManagedReference //when I call user, you do NOT need to show account. We can NOT use JsonIgnore here,it will break.
     private Account account;
 
     public User(String email, String password, String username) {
